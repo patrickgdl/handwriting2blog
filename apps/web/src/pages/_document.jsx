9fee1a60a080dd43d42a8f51c35634057d0db00e
Patrick Lima <patrickgdlima@gmail.com>
2022-10-31 19:04:29 -0300

    chore: favicon

--- apps/web/src/pages/_document.jsx
+++ apps/web/src/pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&display=swap"
           rel="stylesheet"

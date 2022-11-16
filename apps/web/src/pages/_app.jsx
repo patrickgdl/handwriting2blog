92528a4c693eff092266533155a81d9a2eae0ad5
Patrick Lima <patrickgdlima@gmail.com>
2022-11-16 10:49:56 -0300

    feat: context to use supabase

--- apps/web/src/pages/_app.jsx
+++ apps/web/src/pages/_app.jsx
@@ -1,7 +1,21 @@
 import "../../styles/globals.css";
 
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import React from "react";
+
+import { MyUserContextProvider } from "../components/lib/utils";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [supabaseClient] = React.useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <MyUserContextProvider>
+        <Component {...pageProps} />
+      </MyUserContextProvider>
+    </SessionContextProvider>
+  );
 }
 
 export default MyApp;
